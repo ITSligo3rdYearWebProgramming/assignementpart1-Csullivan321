45b033727ece27d364c286956149787aa1f7a0dc
Christopher Sullivan <S00188845@mail.itsligo.ie>
2020-10-25 22:11:37 +0000

    Attempt to delete car from db using a get

--- Cardb.js
+++ Cardb.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = 3000
 
+let cars = [];
+
 mongoose.connect('mongodb://localhost:27017/CarDatabase', {
     "useNewUrlParser": true,
     "useUnifiedTopology": true
@@ -31,11 +33,31 @@ app.get('/addCar/:make', (req, res) => {
     const aCar = new car({make: req.params.make, model: req.params.model, year: req.params.year, fuel: req.params.fuel});
 
     aCar.save()
-    .then((result) => res.send(`${req.params.make } ${req.params.model} was saved to the database`))
+    .then((result) => res.send(`${req.params.make, req.params.model} was saved to the database`))
     .catch((err) =>
     console.err(err));
 
 });
+
+app.get('/deleteCar/:make', (req, res) => {
+    var type = req.params.make;
+
+    cars.remove({
+        make: type
+    }, function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.send("Removed from the database");
+        }
+    });
+})
+
+
+app.listen(port, () => console.log(`Example app listening on 
+  : ${port}!`))
+
 
 
 
